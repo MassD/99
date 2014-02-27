6abfabb7665007d649426f95590c8d2ec0f6fbdb
Jack Chen <massd.me@gmail.com>
2014-02-27 21:24:22 +0000

    continue

--- 80-89+94-graphs/p83.ml
+++ 80-89+94-graphs/p83.ml
@@ -23,16 +23,7 @@ let g1 =
 	    CharMap.add 'k' ['f'] (
 	      CharMap.singleton 'd' []))))))
 
-let cycles g a =
-  let rec collect s marked step =
-    if s = a && step <> 0 then [[s]]
-    else if CharSet.mem s marked then []
-    else 
-      let edges = CharMap.find s g in
-      let m = CharSet.add s marked in
-      List.fold_left (fun acc c -> List.rev_append (collect c m (step+1)) acc) [] edges |> List.map (fun x -> s::x)
-  in 
-  collect a (CharSet.empty) 0
+
 
 	
  
