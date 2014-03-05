1b603f8f087c0305ad3fa59ffad5545942e7637b
Jack Chen <massd.me@gmail.com>
2014-03-05 01:32:49 +0000

    continue p86

--- 80-89+94-graphs/p86.ml
+++ 80-89+94-graphs/p86.ml
@@ -14,8 +14,22 @@ type 'a grpah = { nodes: 'a list; edges: ('a * 'a) list}
 
 let degree g v = List.filter (fun (x,y) -> x = v || y = v) g.edges |> List.length
 
-let sorted_nodes_dg g =
+let sorted_vs_dg g =
   List.map (fun v -> (v,degree g v)) g.nodes |> List.sort (fun (v1,d1) (v2,d2) -> -(compare d1 d2))
 
+let rec (<>) i j = 
+  if i > j then j |<>| i
+  else 
+    let rec aux acc k =
+      if k < i then acc
+      else aux (k::acc) (k-1)
+    in 
+    aux [] j
+
+let coloring g =
+  let sv = sorted_vs_dg g in
+  let (_,d) = List.hd sv in
+  let colors = 1 <> d+1 in
+  
 
 
