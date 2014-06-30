4a84f04332eea9db03fb1abe5b8fbf3022edf4d1
massd <massd.me@gmail.com>
2014-06-30 16:20:30 +0100

    better p89

--- 80-89+94-graphs/p89.ml
+++ 80-89+94-graphs/p89.ml
@@ -8,7 +8,7 @@ Write a predicate that finds out whether a given graph is bipartite
 
 type 'a grpah = { nodes: 'a list; edges: ('a * 'a) list}
 
-let neighbours es v =
+(*let neighbours es v =
   let rec collect acc = function
   | [] -> acc
   | (x,y)::tl -> 
@@ -18,11 +18,11 @@ let neighbours es v =
   in 
   collect [] es
 
-let rev_concat_map f l = List.fold_left (fun acc x -> List.rev_append (f x) acc) [] l
+let rev_concat_map f l = List.fold ~f:(fun acc x -> List.rev_append (f x) acc) ~init:[] l
 
-let (--) l1 l2 = List.filter (fun x -> not (List.mem x l2)) l1
+let (--) l1 l2 = List.filter ~f:(fun x -> not (List.mem x l2)) l1
 
-let is_overlap l1 l2 = List.filter (fun x -> List.mem x l2) l1 |> List.length > 0
+let is_overlap l1 l2 = List.filter ~f:(fun x -> List.mem x l2) l1 |> List.length > 0
 
 let rm_dup l =
   let rec aux acc = function
@@ -40,8 +40,32 @@ let bipartite g =
     else breath_fst nvs q (List.rev_append nvs p)
   in 
   let vs = [List.hd g.nodes] in
-  breath_fst vs [] vs
+  breath_fst vs [] vs*)
 
+
+let find_end g x =
+  List.fold ~init:[] ~f:(fun acc (a,b) -> if a = x then b::acc else if b = x then a::acc else acc) g.edges
+
+let is_overlap l1 l2 = List.filter ~f:(fun x -> List.mem l2 x) l1 |> List.length > 0
+let swap = function None -> None | Some (x,y) -> Some (y,x)
+
+let bipartite_better g =
+  let rec build_from xs (sl, sr) =
+    List.fold ~init:(Some (sl, sr)) ~f:(fun acc x ->
+      match acc with
+	| None -> None
+	| Some (sl, sr) ->
+	  if List.mem sl x || List.mem sr x then Some (sl, sr)
+	  else 
+	    let ys = find_end g x in
+	    if is_overlap ys sl then None
+	    else build_from ys (sr, x::sl) |> swap
+    ) xs
+	
+  in 
+  build_from g.nodes ([], [])
+				      
+      
   
 
 let g1 = 
@@ -55,10 +79,16 @@ let g1 =
 	     'c','h';
 	     'd','h';
 	     'e','a';
-	     'e','f'
+
 	    ]
   }
-    
+   
+let g2 = 
+  {
+    nodes = ['a';'b';];
+    edges = ['a','b';
+	    ]
+  }
     
   
 
